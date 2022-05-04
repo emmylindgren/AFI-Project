94b0ade7edd2b64172142101c222168fd39f18f4
ChristofferBillman <christoffer.billman@gmail.com>
2022-05-04 15:37:19 +0200

    Minor changes.

--- ClientApp/src/components/LogIn.js
+++ ClientApp/src/components/LogIn.js
@@ -5,6 +5,7 @@ import GoogleSignInButton from "./GoogleSignInButton.js";
 import { Link } from "react-router-dom";
 
 import '../custom.css';
+import SignUp from './SignUp';
 
 export class LogIn extends Component {
     static displayName = LogIn.name;
@@ -12,6 +13,7 @@ export class LogIn extends Component {
     render () {
         return (
             <div id="login-page">
+                <SignUp/>
                 <img src={logo} id="logo"></img>
                 <h4 id="login-text">Explore your neighbourhood today!</h4>
                 <div class="google-button"><GoogleLogInButton /></div>
