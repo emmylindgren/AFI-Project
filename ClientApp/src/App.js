054b2d5f31b24b30d98614eaf68002d8df689c74
ChristofferBillman <christoffer.billman@gmail.com>
2022-05-05 16:27:14 +0200

    Removed unused imports.

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
 import { LogIn } from './components/LogIn';
 import SignUp from './components/SignUp';
 import EventCard from './components/EventCard'
