da541939f85a746dc418d73b0056dfd056f8133a
ChristofferBillman <christoffer.billman@gmail.com>
2022-05-09 21:00:35 +0200

    Refactored submitProfile function.

--- ClientApp/src/components/SignUp.js
+++ ClientApp/src/components/SignUp.js
@@ -10,6 +10,7 @@ import { API_ADRESS } from '../config';
 const style = {
     backgroundColor: 'rgb(240,240,240)',
 }
+
 function SignUp() {
 
     const location = useLocation();
@@ -24,6 +25,7 @@ function SignUp() {
     const [profilePicture, setProfilePicture] = useState('plant.png');
 
     const [error, setError] = useState('');
+    const [success, setSuccess] = useState('');
 
     useEffect(() =>{
         const filepicker = document.getElementById('propicker');
@@ -32,45 +34,53 @@ function SignUp() {
         })
     },[])
 
-    const submitProfile = () =>{
-
-        console.log("begin submit")
+    const submitProfile = async () =>{
 
         const reader = new FileReader();
 
         let form = new FormData();
 
-        reader.addEventListener('load', () => {
-            let file = new File([reader.result],'uploadFile.jpg',{type: 'image/jpeg'})
-            form.append('uploadFile', file);
+        const propicker = document.getElementById('propicker');
 
-            form.append('userdata', JSON.stringify({
-                Pr_Firstname: firstname,
-                Pr_Lastname: lastname,
-                Pr_BirthDate: birthdate,
-                Pr_Adress: adress,
-                Pr_PostalCode: postalcode,
-                Pr_City: city,
-            }));
-    
-            axios({
-                method: 'POST',
-                url: API_ADRESS + '/api/profile',
-                data: form,
-            })
-            .then(res => {
-            console.log(res);
-            })
-            .catch(function (error){
-                console.log(error);
-            });
+        form.append('userdata', JSON.stringify({
+            Pr_Firstname: firstname,
+            Pr_Lastname: lastname,
+            Pr_BirthDate: birthdate,
+            Pr_Adress: adress,
+            Pr_PostalCode: postalcode,
+            Pr_City: city,
+        }));
+
+        let result;
+        // If a file is selected
+        if(propicker.files[0]) {
+            // Get from user selection.
+            result = await FileReaderPromised(propicker.files[0])
+        }
+        else{
+            // Get from Google/default.
+            setError('Select a profile picture. Default from google not working yet.')
+            setSuccess('')
+            result = await FileReaderPromised(profilePicture)
+        }
+
+        let file = new File([result],'uploadFile.jpg',{type: 'image/jpeg'})
+        form.append('uploadFile', file);
+
+        axios.post(API_ADRESS + '/api/profile', form)
+        .then(res => {
+            if(res.status >= 200 && res.status < 300){
+                setError('');
+                setSuccess('Successfully created profile.');
+                // Redirect to homepage.
+            }
+        })
+        .catch( err => {
+            setSuccess('')
+            setError('Something went wrong! Try again later.')
+            console.error(err);
         });
 
-        const propicker = document.getElementById('propicker');
-        if(!propicker.files[0]){
-            console.error("Please pick a file. Dumb bitch.");
-            return
-        }
         reader.readAsArrayBuffer(propicker.files[0]);
     }
 
@@ -80,9 +90,7 @@ function SignUp() {
                 <BackButton text="Back" to='/' onClick={() => {console.log("hej!")}}/>
                 <h1>Create New Profile</h1>
 
-                
-                {
-                getProfilePicture(profilePicture)}
+                {getProfilePicture(profilePicture)}
                 
                 {getInput('Firstname','Emma...',firstname,setFirstname)}
                 {getInput('Lastname','Hornham...',lastname,setLastname)}
@@ -91,9 +99,11 @@ function SignUp() {
                 {getInput('Postal Code','907 40.',postalcode,setPostalCode)}
                 {getInput('City','Umea...',city,setCity)}
 
-                <p>{error}</p>
-
                 <Button text='Sign up' buttonColorChoice='green' onClick={() => submitProfile()}/>
+                
+                <div style={{margin: '20px 0 20px 0'}}></div>
+                <p className='err-text'>{error}</p>
+                <p className='success-text'>{success}</p>
 
             </div>
         </div>
@@ -114,13 +124,30 @@ function getInput(label, placeholder,state,setState){
     )
 }
 
+const propicStyle={
+    width: '20rem',
+    height: '20rem',
+    borderRadius: '500px',
+    objectFit: 'cover',
+}
+
 function getProfilePicture(picture){
     return (
-        <div>
-            <img src={picture} id='propic' alt='propic' style={{width: '4rem', height: '4rem', borderRadius: '500px'}}/>
-            <input type='file' id='propicker' className='clickable-text' name='filename'/>
+        <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+            <img src={picture} id='propic' alt='propic' style={propicStyle}/>
+            <input type='file' id='propicker' className='clickable-text' name='filename' style={{width: '145px', paddingTop: '20px'}}/>
         </div>
     )
+}
+
+function FileReaderPromised(file) {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = () => reject(reader.error);
+        reader.readAsArrayBuffer(file);
+        }
+    );
 }
 
 export default SignUp;
