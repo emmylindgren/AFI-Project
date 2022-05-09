827875d94bdbee27c9fe50eae3a81275ad2ba714
ChristofferBillman <christoffer.billman@gmail.com>
2022-05-09 22:13:05 +0200

    Configured to work better with iphone PWA.

--- ClientApp/src/components/Layout.js
+++ ClientApp/src/components/Layout.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Container } from 'reactstrap';
-import { NavMenu } from './NavMenu';
+import '../custom.css'
 
 export class Layout extends Component {
   static displayName = Layout.name;
@@ -10,9 +10,15 @@ export class Layout extends Component {
       <div>
      
         <Container>
+          <div className='notch-bg'></div>
+          {isRunningStandalone() ? <div style={{height: '50px'}}></div> : ''}
           {this.props.children}
         </Container>
       </div>
     );
   }
+}
+
+function isRunningStandalone() {
+  return (window.matchMedia('(display-mode: standalone)').matches);
 }
