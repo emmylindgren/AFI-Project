b433d537044ca88e6621198d6a9580bfeeed1295
alexwahlroos <alex.wahlroos@hotmail.com>
2022-05-10 12:55:41 +0200

    Worked on event card. Only left to do is to use it with the API

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -5,6 +5,8 @@ import { Layout } from './components/Layout';
 import { LogIn } from './components/LogIn';
 import SignUp from './components/SignUp';
 import EventCard from './components/EventCard'
+import Explore from './components/Explore'
+
 
 import './custom.css'
 
@@ -15,6 +17,7 @@ export default function App(){
         <Route exact path='/' element={<LogIn/>} />
         <Route path='/sign-up' element={<SignUp/>} />
         <Route path='/event-card' element={<EventCard/>} />
+        <Route path='/explore' element={<Explore/>} />
       </Routes>
     </Layout>
   );
