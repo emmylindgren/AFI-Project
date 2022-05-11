3e997af3dd3dbe83f6763d59a73212233f5a6f35
ChristofferBillman <christoffer.billman@gmail.com>
2022-05-11 19:57:48 +0200

    TemplatePage layout and style completed.

--- ClientApp/src/components/EventTemplate.js
+++ ClientApp/src/components/EventTemplate.js
@@ -7,14 +7,30 @@ const container = {
     justifyContent: 'space-between',
     alignItems: 'center',
 }
+const iconStyle = {
+    width: '50px',
+    height: '50px',
+    marginRight: '10px',
+    borderRadius: '100%',
+    padding: '5px',
+    backgroundColor: 'var(--light-green)',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+}
 
-function EventTemplate({title,description,suitableFor}) {
+function EventTemplate({title,description,suitableFor,icon}) {
     return (
     <div style={container}>
         <div>
-            <h3>{title}</h3>
-            <p className='muted-text'>{description}</p>
-            <h3 className='muted-text'>Suitable for:</h3>
+            <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+                <div style={iconStyle}>
+                    <img src={icon} width='25px'/>
+                </div>
+                <h3>{title}</h3>
+            </div>
+            <p className='muted-text' style={{margin: '10px 0px 5px 0px'}}>{description}</p>
+            <h3 className='muted-text' style={{marginBottom: '5px'}}>Suitable for:</h3>
             <p className='muted-text'>{suitableFor}</p>
         </div>
         <Button text='Create' buttonColorChoice='green' iconChoice ="add" />
--- ClientApp/src/components/TemplatePage.js
+++ ClientApp/src/components/TemplatePage.js
@@ -7,6 +7,7 @@ const EventTemplates = {
     display: 'flex',
     flexDirection: 'column',
     gap: '20px',
+    marginTop: '40px',
 }
 
 function TemplatePage() {
@@ -15,11 +16,25 @@ function TemplatePage() {
         <div className='page-content'>
             <BackButton text='Explore' to='/'/>
             <h1>New Event</h1>
-            <h3>Presets</h3>
             <Button text='Create Without Preset' buttonColorChoice='green' iconChoice ="add"/>
 
             <div style={EventTemplates}>
-            <EventTemplate title='Promenade' description='A stroll in the park.' suitableFor={["Test", "Test3"]}/>
+            <h3>Presets</h3>
+            <EventTemplate title='Promenade'
+                description='A stroll in the park.'
+                suitableFor={["Test", "Test3"]}
+                icon='icons/PromenadeIcon.svg'
+            />
+            <EventTemplate title='Knitting party'
+                description='Hang on to your knitting skills!'
+                suitableFor={["Test", "Test3"]}
+                icon='icons/KnittingIcon.svg'
+            />
+            <EventTemplate title='Card Game'
+                description='Play a card game with your friends!'
+                suitableFor={["Test", "Test3"]}
+                icon='icons/CardGameIcon.svg'
+            />
             </div>
         </div>
     </div>);
