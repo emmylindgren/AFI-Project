9e5bb00d8b04e08b485dcc919969d40db7545ade
ChristofferBillman <christoffer.billman@gmail.com>
2022-05-12 18:21:05 +0200

    DB Migration. Added ApiKey column to Profile Table and Model.

---
+++ Migrations/20220512150932_ApiKeyInProfileModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AFI_Project.Migrations
+{
+    public partial class ApiKeyInProfileModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ApiKey",
+                table: "Tbl_Profiles",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ApiKey",
+                table: "Tbl_Profiles");
+        }
+    }
+}
--- Models/ProfileModel.cs
+++ Models/ProfileModel.cs
@@ -17,6 +17,8 @@ namespace AFI_Project.Models
         public string Pr_Img { get; set; } = String.Empty;
         public string GoogleId { get; set; } = String.Empty;
         public string Pr_GoogleIdSalt { get; set; } = String.Empty;
+        // Is Hash(GoogleId, Pr_GoogleIdSalt)
+        public string ApiKey { get; set; } = String.Empty;
 
         public virtual ICollection<RequestedInviteModel>? Pr_RequestedInviteModel { get; set; }
         public virtual ICollection<DeclinedInviteModel>? Pr_DeclinedInviteModel { get; set; }
