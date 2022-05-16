5fc712e3612490c0bdf8334529200430e7a4917a
alexwahlroos <alex.wahlroos@hotmail.com>
2022-05-16 10:27:46 +0200

    Removed form tag from search.

--- ClientApp/src/components/Search.js
+++ ClientApp/src/components/Search.js
@@ -1,19 +1,16 @@
-const Search = ({ searchQuery, setSearchQuery }) => (
-        <form action="/" method="get">
-            <div className="wrapper">
-                <div className="search-icon">
-                    <img src="icons/search-icon.svg"></img>
-                </div>        
-                    <input
-                        value={searchQuery}
-                        onInput={e => setSearchQuery(e.target.value)}
-                        type="text"
-                        id="header-search"
-                        placeholder="Search events"
-                        name="s" 
-                    />
-            </div>
-        </form>
+const Search = ({ searchQuery, setSearchQuery }) => (  
+    <div className="wrapper">
+        <div className="search-icon">
+            <img src="icons/search-icon.svg"></img>
+        </div>        
+            <input
+                value={searchQuery}
+                onInput={e => setSearchQuery(e.target.value)}
+                type="text"
+                id="header-search"
+                placeholder="Search events"
+                name="s" 
+            />
+    </div>
 );
-
 export default Search;
