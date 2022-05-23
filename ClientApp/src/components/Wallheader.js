80a8f93bc8d4a5bc3b9e9dc1ec6408ee5dd016e5
ChristofferBillman <christoffer.billman@gmail.com>
2022-05-23 14:55:21 +0200

    Completed visual part of WallHeader and WallPost.

--- ClientApp/src/components/Wallheader.js
+++ ClientApp/src/components/Wallheader.js
@@ -1,18 +1,21 @@
-import React from 'react'
-import { useState , useRef} from "react";
+import React, { useEffect } from 'react'
+import { useState, useRef } from "react";
 import WallPost from "../components/form/WallPost";
+import Button from '../components/Button';
 import { API_ADRESS } from "../config";
 import axios from "axios";
 
 const headerWrapperStyle = {
-    height: '40vw',
-    maxHeight: '200px',
-    
+  height: '200px',
+  backgroundColor: 'var(--white)',
+  padding: '20px',
+  transition: '200ms',
+  borderRadius: '0 0 30px 30px',
 }
 
 const prImgStyle = {
   borderRadius: '100%',
-  margin:'32px 15px 0 0',
+  margin: '32px 15px 0 0',
   width: '45px',
   height: '45px'
 }
@@ -24,46 +27,80 @@ function Wallheader() {
   const [error, setError] = useState('')
   const [success, setSuccess] = useState('')
 
-  const submitPost = async () =>{
+  const [focused, setFocused] = useState(false)
+
+  useEffect(() => {
+    // On component unmount, it runs the function useEffect returns.
+    return () => {
+      document.removeEventListener('click', e => { })
+    }
+  })
+
+  const submitPost = async () => {
 
     let form = new FormData();
 
-    axios.get(API_ADRESS + '/api/')
-
     form.append('postdata', JSON.stringify({
-        Po_Content: content
+      Po_Content: content
     }));
 
     axios.post(API_ADRESS + '/api/post', form)
-    .then(res => {
-        if(res.status >= 200 && res.status < 300){
-            setError('');
-            setSuccess('Successfully posted.');
+      .then(res => {
+        if (res.status >= 200 && res.status < 300) {
+          setError('');
+          setSuccess('Successfully posted.');
         }
-        else{
-            setError('Something went wrong. Try again later.');
-            setSuccess('');
+        else {
+          setError('Something went wrong. Try again later.');
+          setSuccess('');
         }
-    })
-    .catch( err => {
+      })
+      .catch(err => {
         setSuccess('')
         setError('Something went REALLY wrong! Try again later.')
         console.error(err);
-    });
-}
+      });
+  }
+
+  const handlePress = () => {
+    document.addEventListener('click', e => {
+      if (document.getElementById('postHeader').contains(e.target)) setFocused(true)
+      else setFocused(false)
+    })
+  }
 
   return (
-    <div className='page-container'>
-      <div className='page-content'>
-        <div style={headerWrapperStyle} >
-          <h1>Your neighbourhood</h1>
-          <div style={{display:'flex'}}>
-            <img src={API_ADRESS + "/api/profile/image/" + localStorage.getItem("profileId")} style={prImgStyle}></img>  
-            <WallPost value={content} onChange={setContent} label="Post something" placeholder="What have you been up to lately..."/>
+
+    <>
+      <div style={focused ? { ...headerWrapperStyle, height: '330px' } : headerWrapperStyle} onClick={handlePress} id='postHeader'>
+        <h1>Your neighbourhood</h1>
+        <div>
+          <div style={{ display: 'flex' }} >
+
+            <img src={API_ADRESS + "/api/profile/image/" + localStorage.getItem("profileId")} style={prImgStyle}></img>
+            <WallPost
+              value={content}
+              onChange={setContent}
+              label="Post something"
+              placeholder="What have you been up to lately..."
+              open={focused}
+            />
           </div>
+          {focused ? (
+            <div style={{ display: 'flex', justifyContent: 'flex-end', paddingRight: '20px' }}>
+              <div style={{ marginTop: '20px', width: '100px' }}>
+                <Button text="Post" onClick={submitPost} buttonColorChoice="green" iconChoice="post" />
+              </div>
+            </div>
+          ) : (
+            ''
+          )}
+          <p className='err-text'>{error}</p>
+          <p className='success-text'>{success}</p>
         </div>
+
       </div>
-    </div>
+    </>
 
   )
 }
