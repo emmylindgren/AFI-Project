617122443398b0c8f1bb8a71058597cf2c112849
Emmy Lindgren <emmy.lindgren@hotmail.com>
2022-05-23 14:56:42 +0200

    Comments now working. Can like and will display in time order.

--- ClientApp/src/pages/Comments.js
+++ ClientApp/src/pages/Comments.js
@@ -10,7 +10,6 @@ import BackButton from '../components/BackButton';
 import Comment from '../components/Comment';
 
 function Comments() {
-
     const { postID } = 5;
 
     const [post, setPost] = useState([]);
@@ -26,7 +25,6 @@ function Comments() {
         axios.get(API_ADRESS + '/api/Post/'+5)
         .then(res =>{
             setPost(res.data)
-            console.log(res.data)
             setPostState('loaded')
            
         })
@@ -34,13 +32,10 @@ function Comments() {
             setPostState('error')
             console.log(err)
         })
-
-        //api/Comment/fromPost/5
         
         axios.get(API_ADRESS + '/api/Comment/fromPost/'+5)
         .then(res =>{
             setComments(res.data)
-            console.log(res.data)
             setCommentState('loaded')
            
         })
@@ -61,7 +56,7 @@ function Comments() {
             case 'loading':
                 return <LoadingCard/>
             case 'loaded':
-                return <Post post={post} withComment={false}/>
+                return <div style ={{marginBottom:'2rem',}}><Post post={post} withComment={false}/></div>
             case 'error':
                 return <ErrorCard
                 iconChoice={'filenotfound'} 
@@ -90,9 +85,11 @@ function Comments() {
               <div style={{marginBottom:'2rem',}}>
                 <BackButton text={"Your Neighbourhood"} to={'../Wall'}/>
               </div>
-              {getCurrentPostState()}
+                {getCurrentPostState()}
+                <div style={{backgroundColor:'var(--white)'}}><p>Här ska man kunna adda en comment hihih</p></div>
               <div>
-              {getCurrentCommentState()}
+                <h1 style={{color:'var(--black)'}}>Comments</h1>
+                {getCurrentCommentState()}
               </div>
           </div>
     </div>
